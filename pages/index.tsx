305fbf431a1c89af69c2656cff7d7a9b52cc508c
tonnyrogers <antoniel15975@gmail.com>
2020-08-26 21:32:31 -0300

    Basic Config

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Next App</title>
+      </Head>
+
+      <main >
+        <h1>Hello Tony</h1>
+      </main>
+    </div>
+  )
+}
