0bf47f5db548b6f67edc166047455e4cda037398
tonnyrogers <antoniel15975@gmail.com>
2020-09-22 13:20:09 -0300

    Custom Page Style / Finish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Head from 'next/head';
 
 import tonyImage from '../assets/open-peeps.png'
+import { Container } from '../styles/pages/Home';
 
 const Home: React.FC = () => (
-  <div>
+  <Container>
     <Head>
       <title>Next App</title>
     </Head>
 
-    <main>
       <h1>Hello Tony</h1>
       <img src={tonyImage} alt="Tony Man"/>
-    </main>
-  </div>
+      <p>Oi eu sou Tony Amaral</p>
+  </Container>
 );
 
 export default Home;
